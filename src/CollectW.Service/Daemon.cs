4e5c22046466e1885913158af7a24e1d74975d6c
Jose Felix <jose.felix@constant.com.br>
2015-04-07 19:25:53 -0300

    Created service project with installers

---
+++ src/CollectW.Service/Daemon.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceProcess;
+using CollectW.Config;
+
+namespace CollectW.Service
+{
+    public partial class Daemon : ServiceBase
+    {
+        private Configuration _configuration;
+        private Collector _collector;
+        public Daemon()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _configuration=new Configuration();
+            _collector = _configuration.CreateCollector();
+            _collector.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _collector.Stop();
+            _collector.Dispose();
+            _configuration.Dispose();
+        }
+    }
+}
---
+++ src/CollectW.Service/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CollectW.Service
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CollectWInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.CollectWServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // CollectWInstaller
+            // 
+            this.CollectWInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.CollectWInstaller.Password = null;
+            this.CollectWInstaller.Username = null;
+            // 
+            // CollectWServiceInstaller
+            // 
+            this.CollectWServiceInstaller.Description = "Performance counter collector and forwarder";
+            this.CollectWServiceInstaller.DisplayName = "CollectW";
+            this.CollectWServiceInstaller.ServiceName = "CollectW";
+            this.CollectWServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.CollectWInstaller,
+            this.CollectWServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller CollectWInstaller;
+        private System.ServiceProcess.ServiceInstaller CollectWServiceInstaller;
+    }
+}
