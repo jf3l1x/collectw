5ec6e221cc4709a1073060245f6c457fbde16f55
Jose Felix <jose.felix@constant.com.br>
2015-04-07 20:07:53 -0300

    Comment removed

--- src/CollectW.Service/Program.cs
+++ src/CollectW.Service/Program.cs
@@ -4,9 +4,6 @@ namespace CollectW.Service
 {
     internal static class Program
     {
-        /// <summary>
-        ///     The main entry point for the application.
-        /// </summary>
         private static void Main()
         {
             var servicesToRun = new ServiceBase[]
