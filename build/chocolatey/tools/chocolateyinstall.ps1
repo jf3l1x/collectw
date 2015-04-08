bb818fed522cf0ef28490b4e97ae78e02476a36b
Jose Felix <jose.felix@constant.com.br>
2015-04-08 18:12:19 -0300

    chocolatey running but... still some doubts...

--- build/chocolatey/tools/chocolateyinstall.ps1
+++ build/chocolatey/tools/chocolateyinstall.ps1
@@ -4,5 +4,25 @@ $packageName = 'collectw.service' # arbitrary name for the package, used in mess
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $psFile = Join-Path $toolsdir "Install-Service.ps1"
-Install-ChocolateyPowershellCommand $packageName $psFile
+$serviceName = "collectw"
+$exePath = Join-Path $toolsdir "collectw.service.exe"
+
+$existingService = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
+
+if ($existingService) 
+{
+  "'$serviceName' exists already. Stopping."
+  Stop-Service $serviceName
+  "Waiting 3 seconds to allow existing service to stop."
+  Start-Sleep -s 3
+
+  $existingService.Delete()
+  "Waiting 5 seconds to allow service to be uninstalled."
+  Start-Sleep -s 5  
+}
+
+"Installing the service."
+New-Service -BinaryPathName $exePath -Name $serviceName -DisplayName $serviceName -Description "Performance counters collector and forwarder" -StartupType Automatic 
+"Service was installed sucessfully."
+
 
--- build/chocolatey/tools/chocolateyuninstall.ps1
+++ build/chocolatey/tools/chocolateyuninstall.ps1
@@ -3,13 +3,25 @@
 $packageName = 'collectw.service'
 $registryUninstallerKeyName = 'collectw.service' #ensure this is the value in the registry
 $validExitCodes = @(0)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$serviceName = "collectw"
+$exePath = Join-Path $toolsdir "collectw.service.exe"
 
-#$osBitness = Get-ProcessorBits
+$existingService = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
 
-#if ($osBitness -eq 64) {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#} else {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#}
-#
-#Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+if ($existingService) 
+{
+  
+  Stop-Service $serviceName
+  "Waiting 3 seconds to allow existing service to stop."
+  Start-Sleep -s 3
+
+  $existingService.Delete()
+  "Waiting 5 seconds to allow service to be uninstalled."
+  Start-Sleep -s 5  
+}
+
+"Service Uninstalled"
+
+
+
